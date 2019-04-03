95ca4547923e15148d38cc331620e95e43b215d8
joseph bourgoin <jabourg@gmail.com>
2019-04-03 12:18:01 -0700

    adding class files -p210

---
+++ p210/JB_WinForm/JB_WinForm/Note.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB_WinForm
+{
+    public class Note
+    {
+        private DateTime _date;
+        private static int _NoteNumber = 0;
+        private string _text = "";
+
+        public Note(string newText)  // constructor sets the Date field to "now"
+        {
+            _date = DateTime.UtcNow;
+            _NoteNumber++; // bump up our count of Notes
+            _text = newText; // set the text
+        }
+
+        //  Class Properties
+        public string NoteNumber // each "record" gets a new, unique NoteNumber
+        {
+            get { return _NoteNumber.ToString(); }
+        }
+        public string Text // the text for our Note
+        {
+            get { return _text; }
+        }
+        public DateTime Date  // can read this property, but it is set only in constructor
+        {
+            get { return _date.ToLocalTime(); }
+        }
+    }
+
+}
