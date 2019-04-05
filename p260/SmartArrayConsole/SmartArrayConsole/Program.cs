1130e0c6e00803037a37ac758eaeff1489b21561
joseph bourgoin <jabourg@gmail.com>
2019-04-04 18:40:51 -0700

    adding p260 folder and files

---
+++ p260/SmartArrayConsole/SmartArrayConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SmartArrayConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartArray mySmartArray = new SmartArray(10);
+            mySmartArray.SetAtIndex(2, 102);
+            mySmartArray.SetAtIndex(4, 104);
+            mySmartArray.PrintAllElements();
+            Console.WriteLine();
+
+            int x = mySmartArray.GetAtIndex(4);
+            Console.WriteLine("x is {0}", x);
+            Console.WriteLine();
+
+            bool found = mySmartArray.Find(102);
+            Console.WriteLine(found);
+
+            mySmartArray.Resize(5);
+            mySmartArray.SetAtIndex(2, 102);
+
+            mySmartArray.PrintAllElements();
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ p260/SmartArrayConsole/SmartArrayConsole/SmartArray.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartArrayConsole
+{
+    public class SmartArray
+    {
+        int[] UnderlyingArray; // using the C# simple array to hold our data
+
+        public SmartArray(int origSize)  // constructor sets initial size
+        {
+            UnderlyingArray = new int[origSize];
+        }
+
+        public void SetAtIndex(int index, int val)
+        {
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException("too small");
+            }
+            else if (index > UnderlyingArray.Length - 1)
+            {
+                throw new IndexOutOfRangeException("too big");
+            }
+            else
+            {
+                UnderlyingArray[index] = val;
+                return;
+            }
+        }
+
+        public int GetAtIndex(int index)
+        {
+            try
+            {
+                return UnderlyingArray[index];
+            }
+            catch (Exception)
+            {
+
+                if (index < 0)
+                {
+                    throw new IndexOutOfRangeException("too small");
+                }
+                else if (index > UnderlyingArray.Length - 1)
+                {
+                    throw new IndexOutOfRangeException("too big");
+                }
+            }
+            finally
+            {
+
+            }
+            return Int32.MinValue;
+        }
+
+        public void PrintAllElements()
+        {
+            for (int i = 0; i < UnderlyingArray.Length; i++)
+            {
+                Console.WriteLine(UnderlyingArray[i]);
+            }
+        }
+
+        public bool Find(int val)
+        {
+            for (int i = 0; i < UnderlyingArray.Length; i++)
+            {
+                if (UnderlyingArray[i] == val)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Resize(int newsize)
+        {
+            int[] tempArray = new int[newsize];
+            int size = UnderlyingArray.Length;
+
+            if (newsize < size)
+            {
+                //temp array is smaller than UnderlyingArray
+                size = newsize;
+            }
+            else if (tempArray.Length > UnderlyingArray.Length)
+            {
+                //temp array is larger than UnderlyingArray
+                for (int i = 0; i < size; i++)
+                {
+                    tempArray[i] = UnderlyingArray[i];
+                }
+            }
+            //re-assign temp array and values to UnderlyingArray
+            UnderlyingArray = tempArray;
+        }
+
+
+    } // end of SmartArray class
+}
